4cf8847daed9a62d7684c96e6047583beb4bd87f
Ethan <41048224+ezolla@users.noreply.github.com>
2019-03-14 11:00:54 -0700

    Cleaned up codebase

--- Rocket.java
+++ Rocket.java
@@ -1,11 +1,4 @@
-/*
-Ethan Zoller
-10/28/18
-Rocketship!
-Project Description - Java program that creates a print out of a rocket ship, utilizing methods, nested for loops, print/println statements.
-*/
-
-public class Rocket_EZ {
+public class Rocket {
    // Scale Determinator
    public static final int SIZE = 3;
    
@@ -19,30 +12,30 @@ public class Rocket_EZ {
       half1();
       line();
       cone();
-   }// Main Close
+   } // Main Close
    
    // Creates the cone which is used on the top and bottom of the rocket
    public static void cone() {
-		for (int line = 1; line <= (SIZE * 2 - 1); line++) {
+	for (int line = 1; line <= (SIZE * 2 - 1); line++) {
          // starting space of new line
-			System.out.print(" ");
-			for (int spaces = 1; spaces <= (SIZE * 2 - 1) - line; spaces++){
-				System.out.print(" ");
-			}
-         // / pattern (increasing)
-			for (int slashes = 1; slashes <= line; slashes++) {
-				System.out.print("/");
-			}
-         // Middle section **
-			System.out.print("**");
-         // \ pattern (increasing)
-			for (int slashes = 1; slashes <= line; slashes++) {
-				System.out.print("\\");
-			}
-         // ending space that sends to the next line
-			System.out.println(" ");
-		}
-	} // Cone Close
+	System.out.print(" ");
+	for (int spaces = 1; spaces <= (SIZE * 2 - 1) - line; spaces++){
+		System.out.print(" ");
+	}
+        // / pattern (increasing)
+	for (int slashes = 1; slashes <= line; slashes++) {
+		System.out.print("/");
+	}
+        // Middle section **
+	System.out.print("**");
+        // \ pattern (increasing)
+	for (int slashes = 1; slashes <= line; slashes++) {
+		System.out.print("\\");
+	}
+        // ending space that sends to the next line
+		System.out.println(" ");
+	}
+   } // Cone Close
    
    // Creates line used to intersect multiple sections within the rocket
    public static void line() {
@@ -64,17 +57,17 @@ public class Rocket_EZ {
          // Split it in half, print it twice to make full pattern
          for (int twotimes = 1; twotimes <= 2; twotimes++) {
             // Dots (Decreasing)
-				for (int dots = 1; dots <= SIZE - row; dots++) {
-					System.out.print(".");
-				}
+			for (int dots = 1; dots <= SIZE - row; dots++) {
+				System.out.print(".");
+			}
             // /\ (Increasing)
-				for (int slants = 1; slants <= row; slants++) {
-					System.out.print("/\\");
-				}
+			for (int slants = 1; slants <= row; slants++) {
+				System.out.print("/\\");
+			}
             // Dots (Decreasing)
-				for (int dots = 1; dots <= SIZE - row; dots++) {
-					System.out.print(".");
-				}
+			for (int dots = 1; dots <= SIZE - row; dots++) {
+				System.out.print(".");
+			}
          } // Repeat For Loop Close
          // Pipe Close
          System.out.println("|");
@@ -105,46 +98,4 @@ public class Rocket_EZ {
    		System.out.println("|");
    	} 
    } // Half2 Close
-} // Rocket_EZ Close 
-
-/*
-Pseudo Code
------------
-Method Order
-cone();
-line();
-half1();
-half2();
-line();
-half2();
-half1();
-line();
-cone();
-
-cone();
-/ (increasing)
-** 
-\ (increasing)
-
-line();
-+
-=*=* (pattern)
-+
-
-half1();
-|
-.. (descreasing)
-/\ (increasing)
-.... (decreasing)
-/\ (increasing)
-.. (descreasing)
-|
-
-half2();
-
-|
-\/\/\/ (decreasing)
-\/\/\/ (decreasing)
-|
-
-*/
+} // Rocket Close 
